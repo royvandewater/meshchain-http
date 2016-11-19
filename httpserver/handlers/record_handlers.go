1a5d7c552176e818745744c2d42c6a5a435598c7
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-11-19 10:41:46 -0700

    Parse out bearer tokens

--- httpserver/handlers/record_handlers.go
+++ httpserver/handlers/record_handlers.go
@@ -3,20 +3,49 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gocraft/web"
 	"github.com/royvandewater/meshchain/record"
 )
 
+func parseAuthHeader(headers map[string][]string) string {
+	authHeaders, ok := headers["Authorization"]
+	if !ok {
+		return ""
+	}
+
+	authHeader := ""
+	for _, header := range authHeaders {
+		if strings.HasPrefix(header, "Bearer ") {
+			authHeader = header
+			break
+		}
+	}
+
+	if authHeader == "" {
+		return ""
+	}
+
+	return strings.Replace(authHeader, "Bearer ", "", 1)
+}
+
 // CreateRecord handles a POST Record request
 func CreateRecord(rw web.ResponseWriter, req *web.Request) {
+	jwt := parseAuthHeader(req.Header)
+
+	if jwt == "" {
+		respondWithError(rw, 401, fmt.Errorf("Unauthorized"))
+		return
+	}
+
 	rec, err := record.NewFromReader(req.Body)
 	if err != nil {
 		respondWithError(rw, 422, err)
 		return
 	}
 
-	err = rec.Validate()
+	err = rec.Validate(jwt)
 	if err != nil {
 		respondWithError(rw, 422, err)
 		return
